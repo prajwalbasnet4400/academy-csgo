f76b65c3e8e7dbc84e1f8b794b6a5cb41b3504a6
Prajwal Basnet <81688042+prajwalbasnet4400@users.noreply.github.com>
2021-11-27 12:13:16 +0545

    Update urls.py

--- csgo/urls.py
+++ csgo/urls.py
@@ -6,7 +6,7 @@ import debug_toolbar
 urlpatterns = [
     path('',include('stats.urls')),
     path('store/',include('store.urls')),
-    path('admin/', admin.site.urls),
+    path('academy-admin/', admin.site.urls),
     path('', include('social_django.urls', namespace='social')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
