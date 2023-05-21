06cd48d146b0f2c35a56aa034107cd462ddbbd67
Prajwal <prajwal.basnet.asdf@gmail.com>
2023-05-21 13:57:59 +0545

    added sentry integration

--- csgo/settings.py
+++ csgo/settings.py
@@ -4,6 +4,8 @@ import os
 
 dotenv.load_dotenv()
 import dj_database_url
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -164,3 +166,20 @@ KHALTI_API_SECRET_KEY = os.environ.get('KHALTI_API_SECRET_KEY')
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN",""),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=0.5,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
